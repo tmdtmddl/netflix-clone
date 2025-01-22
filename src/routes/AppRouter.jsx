06e484baaebaaabe4540438e05b54745cf22eb31
tmdtmddl <tmddlqkqh@naver.com>
2025-01-22 11:21:16 +0900

    25_01_22

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -12,6 +12,7 @@ import LearnUseMemo from "./LearnReact/learn-useMemo";
 import Todo from "./todo";
 import PDsameple from "./LearnReact/props-drilling/sample1";
 import PropsDrilling2 from "./LearnReact/props-drilling/props-drilling2";
+import RandomNumber from "./radom-number";
 
 const AppRouter = () => {
   return (
@@ -23,6 +24,9 @@ const AppRouter = () => {
           <Route path="faq" element={<FAQ />} />
           <Route path="contact-us" element={<ContactUs />} />
         </Route>
+
+        <Route path="/random-number" element={<RandomNumber />} />
+
         <Route path="/Learn-react">
           <Route index element={<LearnReact />} />
           <Route path="useState" element={<UseState />} />
---
+++ src/routes/radom-number/index.jsx
@@ -0,0 +1,9 @@
+const RandomNumber = () => {
+  return (
+    <div>
+      <h1>RandomNumber</h1>
+    </div>
+  );
+};
+
+export default RandomNumber;
